549aa9b9ebd59ab43387e30dc81b24639e501095
Ara101 <Lionel.tukei@gmail.com>
2022-11-30 16:21:09 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
-# Set up to use the Python package
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read() 
+
+INSTALL_REQUIRES = []
+
+def doSetup(install_requires):
+    setup(
+        name='PCR_protocal_optimizer',
+        version='0.1',
+        author=' Lily Torp and K. Lionel Tukei ',
+        author_email='ltorp3@uw.edu',
+        url='https://github.com/Ara101/PCR_Optimization_class.git',
+        description='Enables intellisense for **kwargs',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=['PCR_Optimization'],
+        package_dir={'PCR_Optimization':
+            'PCR_Optimization'},
+        install_requires=install_requires,
+        include_package_data=True,
+    )
+
+if __name__ == '__main__':
+  doSetup(INSTALL_REQUIRES)
