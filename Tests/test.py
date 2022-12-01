d92de3292bfbaf09696f112823639c218967bdaf
Ara101 <Lionel.tukei@gmail.com>
2022-11-30 19:51:58 -0800

    Update test.py

---
+++ Tests/test.py
@@ -0,0 +1,39 @@
+"""
+Created Fall 2022
+Authors: Lily Torp, K. Lionel Tukei
+"""
+
+import unittest 
+
+def setUp():
+    return
+
+def test0():
+    return
+
+def test1():
+    return
+
+def test2():
+    return
+
+def test3():
+    return
+
+class TestPCR_protocol_optimizer():
+
+    def Testcheck():
+        return
+
+    def TestcheckPrimerGeneCompatability():
+        return
+
+    def TestcountDNAbase():
+        return
+
+    def TestiProofAnalyzer():
+        return
+
+    def TesttaqAnalyzer():
+        return
+
