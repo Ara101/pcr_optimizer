0c889d5ee5f4807521f28bf656c80491b8614c28
Ara101 <Lionel.tukei@gmail.com>
2022-12-02 20:06:21 -0800

    test_PCR_protocol_optimizer

---
+++ PCR_protocol_optimizer/test_PCR_protocol_optimizer.py
@@ -0,0 +1,59 @@
+"""
+Created Fall 2022
+Authors: Lily Torp, K. Lionel Tukei
+"""
+
+import unittest 
+from PCR_protocol_optimizer import pcr
+
+
+class TestPCR_protocol_optimizer(unittest.TestCase):
+
+    def setUp(self):
+        # Annealing positions are standard
+        gene = "atggagacagacacactcctgctatgggtactgctgctctgggttccaggttccactggtgacacaagtttgtacaaaaaagttggcaccaagtcgatcctagatggccttgcagataccaccttccgcaccatcaccactgacctcctgtacgtgggctcaaatgacattcagtacgaagacatcaaaggtgacatggcatccaaattagggtacttcccacagaaattccctttaacttcctttaggggaagtcccttccaagagaagatgactgcgggagacaacccccagctagtcccagcagaccaggtgaacattacagaattttacaacaagtctctctcgtccttcaaggagaatgaggagaacatccagtgtggggagaacttcatggacatagagtgtttcatggtcctgaaccccagccagcagctggccattgcagtcctgtccctcacgctgggcaccttcacggtcctggagaacctcctggtgctgtgcgtcatcctccactcccgcagcctccgctgcaggccttcctaccacttcatcggcagcctggcggtggcagacctcctggggagtgtcatttttgtctacagcttcattgacttccacgtgttccaccgcaaagatagccgcaacgtgtttctgttcaaactgggtggggtcacggcctccttcactgcctccgtgggcagcctgttcctcacagccatcgacaggtacatatccattcacaggcccctggcctataagaggattgtcaccaggcccaaggccgtggtggcgttttgcctgatgtggaccatagccattgtgatcgccgtgctgcctctcctgggctggaactgcgagaaactgcaatctgtttgctcagacattttcccacacattgatgaaacctacctgatgttctggatcggggtcaccagcgtactgcttctgttcatcgtgtatgcgtacatgtatattctctggaaggctcacagccacgccgtccgcatgattcagcgtaccgacgcgctggacctggaggagggaggaaacgtctatatcaaggccgacaagcagaagaacggcatcaaggcgaacttctgcatccgccacaacatcgaggacggcggcgtgcagctcgcctaccactaccagcagaacacccccatcggcgacggccccgtgctgctgcccgacaaccactacctgagcgtgcagtccaaactttcgaaagaccccaacgagaagcgcgatcacatggtcctgctggagttcgtgaccgccgccgggatcactttcggcatggacgagctgtacaagggcggtaccggagggagcatggtgagaaagggcgaggagctgttcaccggggtggtgcccatcctggtcgagctggacggcgacgtaaacggccacaagttcagcgtgggcggcgagggtgagggcgatgccaccgttggcaagctgaccctgaagttcatctgcaccaccggcaagctgcccgtgccctggcccaccctcgtgaccaccctgacctacggcgtgcagtgcttcagccgctaccccgaccacatgaagcagcacgacttcttcaagtccgccatgcccgaaggctacatccaggagcgcaccatcttcttcaaggacgacggcaactacaagacccgcgccgaggtgaagttcgagggcgacaccctggtgaaccgcatcgagctgaagggcatcgacttcaaggaggacggcaacatcctggggcacaagctggagtacaacaccggagcagcagcacgctggcgcgggcggcgcatggacattaggttagccaagaccctggtcctgatcctggtggtgttgatcatctgctggggccctctgcttgcaatcatggtgtatgatgtctttgggaagatgaacaagctcattaagacggtgtttgcattctgcaccatgctctgcctgctgaactccaccgtgaaccccatcatctatgctctgaggagtaaggacctgcgacacgctttccggagcatgtttccctcttgtgaaggcactgcgcagcctctggataacagcatgggggactcggactgcctgcacaaacacgcaaacaatgcagccagtgttcacagggccgcagaaagctgcatcaagagcacggtcaagattgccaaggtaaccatgtctgtgtccacagacacgtctgccgaggctctg"
+        forward_primer = "atggagacagacacactcctgctatgg"
+        reverse_primer = "cagagcctcggcagacgtgt"
+
+        # Annealing positions are different | startr = 10, stopr = 31 , startf = 79, stopf = 60
+        gene0 = "caatatggtgagcaatcttttgctcttggggattgaccacagtcgtcgacgcgatgcttgccctagtaagcttggcgtacaattacaagtgataagaggt"
+        forward_primer0 = "GAGCAATCTTTTGCTCTTGGGG" 
+        reverse_primer0 = "TACGCCAAGCTTACTAGGGC"
+
+        # Forward Primer incompatible, Reverse Primer incompatible
+        gene1 = "acggagatttcccccttcctagttccctaatggataagatgtttaagatgttcaaacaaggatattcactgtgccagccctgtggtcgggaagtcgaata"
+        forward_primer1 = "ACGGAGATTTCCCCCTTCCTAK" 
+        reverse_primer1 = "TTATTCGACTTCCCGACCACAG" 
+
+        self.entry = pcr(gene,forward_primer,reverse_primer)
+        self.entry0 = pcr(gene0,forward_primer0,reverse_primer0)
+        self.entry1 = pcr(gene1,forward_primer1,reverse_primer1)
+        
+    def test_countGCcontent(self):
+        result = self.entry.countGCcontent()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result,tuple)
+        self.assertIsInstance(result[0],str)
+        self.assertIsInstance(result[1],float)
+        self.assertEqual(result[1],56.72990063233966)
+        
+    def test_check(self):
+        result = self.entry.check()
+        result0 = self.entry0.check(startr = 10, stopr = 31 , startf = 79, stopf = 60)
+        result1 = self.entry1.check()
+        positive_result = ["Gene looks good!","Forward primer looks good!","Reverse primer looks good!","Primers and Gene are compatible!"]
+        negative_result = ["Gene looks good!","Unacceptable character in forward primer. Check sequence","Reverse primer looks good!","Both Primers and gene are incompatible. Check annealing location for both primers"]
+
+        self.assertIsNone(result)
+        self.assertEqual(result,print(*positive_result, sep = "\n")) 
+        self.assertEqual(result0,print(*positive_result, sep = "\n"))
+        self.assertEqual(result1,print(*negative_result, sep = "\n"))
+
+    def test_recommend(self):
+        result = self.entry.recommend()
+        self.assertIsNotNone(result)
+        self.assertIsInstance(result,str)
+
+
+if __name__ == '__main__':
+    unittest.main()
